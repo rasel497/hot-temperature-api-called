1b9e68f5d27fcc834e2f8e4b5ec88c7506b93f41
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-04 21:01:18 +0600

    show only temperature value html

--- js/app.js
+++ js/app.js
@@ -3,12 +3,28 @@
 const API_KEY = `0e78555d9f392cf9837b60d1123d1802`;
 
 //load data
-const loadTepmerature = (city) => {
+const loadTepmerature = city => {
     // added city name= city, API key = API_KEY and add &units=metric
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
+        .then(data => displayTemperature(data));
 }
+
+//
+const displayTemperature = data => {
+    const temperature = document.getElementById('temperature');
+    // console.log(data.main.temp);
+    // temperature.innerText = data.main.data;
+    temperature.innerText = data.main.temp;
+}
+
+
+document.getElementById('btn-search').addEventListener('click', function () {
+
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    loadTepmerature(city);
+})
 
 loadTepmerature('dhaka');
